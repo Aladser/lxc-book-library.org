9763b252b1bc1b94aca11a75f7ada42856004abf
Aladser <aladser@gmail.com>
2024-02-06 06:14:15 +0900

    обновление структуры проекта

--- config.php
+++ config.php
@@ -5,25 +5,25 @@ namespace App;
 // --- конфиг сайта ---
 function config($param)
 {
-    $site_address = 'lxc-book-library';
-    // $site_address = 'd420-213-87-102-205.ngrok-free.app';
+    $site_name = 'lxc-book-library';
+    $site_address = $site_name.'.org';
+    // $site_name = 'd420-213-87-102-205.ngrok-free.app';
     // --- список глобальных параметров ---
     $paramList = [
         // подключение к БД
         'HOST_DB' => 'localhost',
-        'NAME_DB' => $site_address,
+        'NAME_DB' => $site_name,
         'USER_DB' => 'admin',
         'PASS_DB' => '@admin@',
         // базовый адрес страницы
-        'SITE_ADDRESS' => "$site_address.org",
+        'SITE_ADDRESS' => $site_address,
         'SITE_NAME' => 'Книжная библиотека',
         // вк-авторизация
-        'VK_REDIRECT_URI' => "http://$site_address.org/user/auth_vk",
+        'VK_REDIRECT_URI' => "http://$site_address/user/auth_vk",
         'VK_CLIENT_ID' => 51613986,
         'VK_CLIENT_SECRET' => 'pL1sG0ctOLPHZiiPtZkj',
-        'VK_VERSION' => 5.154,
         // google-авторизация
-        'GOOGLE_REDIRECT_URI' => "http://$site_address.org/user/auth_google",
+        'GOOGLE_REDIRECT_URI' => "http://$site_address/user/auth_google",
         'GOOGLE_CLIENT_ID' => '682336466170-iab6q6gonoi2qm7rvipgph7f4enk0tbi.apps.googleusercontent.com',
         'GOOGLE_CLIENT_SECRET' => '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',
     ];
