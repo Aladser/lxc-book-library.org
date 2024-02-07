8051bc1b8330c1b5c7cd76cb0c23c25c5bef89b0
Aladser <aladser@gmail.com>
2024-02-07 18:25:35 +0900

    up страница авторов

--- public/js/author.js
+++ public/js/author.js
@@ -7,10 +7,15 @@ window.addEventListener('DOMContentLoaded', function(e) {
     document.oncontextmenu = () => false;
 });
 
+window.addEventListener('click', function(e) {
+    authorContextMenu.classList.remove('author-context-menu--active');
+});
+
 // нажатия правой кнопкой мыши на строке автора
 authorRows.forEach(row => {
     row.addEventListener('contextmenu', function(e) {
         authorContextMenu.style.left = (e.pageX-10)+'px';
         authorContextMenu.style.top = (e.pageY-5)+'px';
+        authorContextMenu.classList.add('author-context-menu--active');
     });
 }); 
