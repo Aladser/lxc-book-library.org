06d6bc08898abf10fb20c05ad60771bd5316623a
Aladser <aladser@gmail.com>
2024-02-10 09:13:48 +0900

    обновлены author.js, genre.js

--- public/js/author.js
+++ public/js/author.js
@@ -1,46 +1,44 @@
 /** блок ошибок */
-const errorPrg = document.querySelector('#prg-error');
+const errorPrg = document.querySelector("#prg-error");
 /** клиентский контроллер авторов */
 const authorClientController = new AuthorClientController(errorPrg);
 
 /** CSRF */
 const csrf = document.querySelector("meta[name='csrf']");
 /** таблица авторов */
-const authorTable = document.querySelector('#author-table').childNodes[1];
+const authorTable = document.querySelector("#author-table").childNodes[1];
 /** базовое CSS имя контекстного меню автора*/
-const authorContextMenuClassName = 'author-context-menu';
+const authorContextMenuClassName = "author-context-menu";
 /** контекстное меню автора */
 const authorContextMenu = new ContextMenu(authorContextMenuClassName);
 
-window.addEventListener('DOMContentLoaded', function(e) {
-    document.oncontextmenu = () => false;
-    window.addEventListener('click', () => authorContextMenu.hide());
+window.addEventListener("DOMContentLoaded", function (e) {
+  document.oncontextmenu = () => false;
+  window.addEventListener("click", () => authorContextMenu.hide());
 
-    // контекстное меню автора
-    document.querySelectorAll('.table-row').forEach(row => {
-        row.addEventListener('contextmenu', e => openContextMenu(e));
-    });
-    // изменить автора
-    document.querySelectorAll('.author-context-menu__btn-edit').forEach(btn => {
-        btn.onclick = () => authorClientController.edit(csrf);
-    });
-    // удалить автора
-    document.querySelectorAll('.author-context-menu__btn-remove').forEach(btn => {
-        btn.onclick = () => authorClientController.destroy(csrf);
-    });
+  // контекстное меню автора
+  document.querySelectorAll(".table-row").forEach((row) => {
+    row.addEventListener("contextmenu", (e) => openContextMenu(e));
+  });
+  // изменить автора
+  document.querySelector(".author-context-menu__btn-edit").onclick = () =>
+    authorClientController.edit(csrf);
+  // удалить автора
+  document.querySelector(".author-context-menu__btn-remove").onclick = () =>
+    authorClientController.destroy(csrf);
 });
 
 /** форма добавления автора*/
-const addAuthorForm = document.querySelector('#form-add-author');
-addAuthorForm.onsubmit = async(e) => {
-    let tr = await authorClientController.store(e, authorTable);
-    tr.oncontextmenu = e => openContextMenu(e);
+const addAuthorForm = document.querySelector("#form-add-author");
+addAuthorForm.onsubmit = async (e) => {
+  let tr = await authorClientController.store(e, authorTable);
+  tr.oncontextmenu = (e) => openContextMenu(e);
 };
 
 /** ПКМ по автору */
 function openContextMenu(e) {
-    addAuthorForm.reset();
-    errorPrg.textContent = '';
-    authorContextMenu.show(e);
-    authorClientController.setSelectedAuthor(e.target);
+  addAuthorForm.reset();
+  errorPrg.textContent = "";
+  authorContextMenu.show(e);
+  authorClientController.setSelectedAuthor(e.target);
 }
--- public/js/genre.js
+++ public/js/genre.js
@@ -1,50 +1,42 @@
 /** блок ошибок */
-const errorPrg = document.querySelector('#prg-error');
+const errorPrg = document.querySelector("#prg-error");
 /** клиентский контроллер жанров */
 const genreClientController = new GenreClientController(errorPrg);
 
-
-/** базовое CSS имя контекстного меню автора*/
-const genreContextMenuClassName = 'genre-context-menu';
-/** контекстное меню автора */
+/** базовое CSS имя контекстного меню жанра*/
+const genreContextMenuClassName = "genre-context-menu";
+/** контекстное меню жанра */
 const genreContextMenu = new ContextMenu(genreContextMenuClassName);
-
 
 /** CSRF */
 const csrf = document.querySelector("meta[name='csrf']");
 /** таблица жанров */
-const genreTable = document.querySelector('#genre-table').childNodes[1];
+const genreTable = document.querySelector("#genre-table").childNodes[1];
 
+window.addEventListener("DOMContentLoaded", function () {
+  document.oncontextmenu = () => false;
+  window.addEventListener("click", () => genreContextMenu.hide());
 
-window.addEventListener('DOMContentLoaded', function() {
-    document.oncontextmenu = () => false;
-    window.addEventListener('click', () => genreContextMenu.hide());
-
-    // контекстное меню жанра
-    document.querySelectorAll('.table-row').forEach(row => {
-        row.oncontextmenu = e => openContextMenu(e);
-    });
-    // изменить жанр
-    document.querySelectorAll('.genre-context-menu__btn-edit').forEach(btn => {
-        btn.onclick = () => genreClientController.edit(csrf);
-    });
-    // удалить жанр
-    document.querySelectorAll('.genre-context-menu__btn-remove').forEach(btn => {
-        btn.onclick = () => genreClientController.destroy(csrf);
-    });
+  // контекстное меню жанра
+  document.querySelectorAll(".table-row").forEach((row) => {
+    row.oncontextmenu = (e) => openContextMenu(e);
+  });
+  // удалить жанр
+  document.querySelector(".genre-context-menu__btn-remove").onclick = () =>
+    genreClientController.destroy(csrf);
 });
 
 /** форма добавления жанра*/
-const addGenreForm = document.querySelector('#form-add-genre');
-addGenreForm.onsubmit = async(e) => {
-    let tr = await genreClientController.store(e, genreTable);
-    tr.oncontextmenu = e => openContextMenu(e);
+const addGenreForm = document.querySelector("#form-add-genre");
+addGenreForm.onsubmit = async (e) => {
+  let tr = await genreClientController.store(e, genreTable);
+  tr.oncontextmenu = (e) => openContextMenu(e);
 };
 
 /** ПКМ по жанру */
 function openContextMenu(e) {
-    addGenreForm.reset();
-    errorPrg.textContent = '';
-    genreContextMenu.show(e);
-    genreClientController.setSelectedGenre(e.target);
+  addGenreForm.reset();
+  errorPrg.textContent = "";
+  genreContextMenu.show(e);
+  genreClientController.setSelectedGenre(e.target);
 }
