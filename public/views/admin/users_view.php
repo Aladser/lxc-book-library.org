266807577c3bdc8f207793ee2de07baad1e0f467
Aladser <aladser@gmail.com>
2024-02-12 18:40:29 +0900

    обновление страницы пользователей администратора

--- public/views/admin/users_view.php
+++ public/views/admin/users_view.php
@@ -6,21 +6,32 @@
         <!-- форма добавления пользователя -->
         <article class='section-mb'>
             <h5>Новый пользователь:</h5>
-            <form id='form-add-user' class='mb-1' method='post' action='<?php echo $routes['store']; ?>'>
-                <input type="email" name="email" placeholder='почта' class='form-add__input theme-border' required>
-                <input type="password" name="password" placeholder='пароль' class='form-add__input theme-border' value='111' required>
-                <select name="is_admin" class='form-add__input theme-bg-сolor-white theme-border pe-4'>
-                    <option value=0>Нет</option>
-                    <option value=1>Да</option>
-                </select>
-                <input type="submit" value="Добавить" class='form-add__input theme-bg-сolor-white theme-border px-4'>
+            <form id='form-add-user' class='form-add-user mb-1' method='post' action='<?php echo $routes['store']; ?>'>
+                <div class='form-add-user__row'>
+                    <label for="email">Почта</label>
+                    <input type="email" name="email" placeholder='почта' class='form-add__input theme-border' required>
+                </div>
+                <div class='form-add-user__row'>
+                    <label for="password">Пароль</label>
+                    <input type="password" name="password" placeholder='пароль' class='form-add__input theme-border' value='111' required>
+                    <p class='ps-3'><i>Пароль по умолчанию: 111</i></p>
+                </div>
+                <div class='form-add-user__row'>
+                    <label for="is_admin">Админ.права</label>
+                    <select name="is_admin" class='form-add__input theme-bg-сolor-white theme-border pe-4'>
+                        <option value=0>Нет</option>
+                        <option value=1>Да</option>
+                    </select>
+                </div>
+
+                <input type="submit" value="Добавить" class='form-add__input theme-bg-сolor-white theme-border px-4 mt-1'>
                 <input type="hidden" name="CSRF_JS" value="<?php echo $data['csrf']; ?>">
             </form>
-            <p><i>Пароль по умолчанию: 111</i></p>
         </article>
 
         <!-- пользователи -->
         <div class='section-mb'>
+            <h5>Пользователи:</h5>
             <table id='user-table' class='w-100'>
                 <tr>
                     <th class='<?php echo $css_tr_style; ?>'>Логин</th>
