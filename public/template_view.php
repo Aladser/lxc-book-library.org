9309f8d8ac5b08732272d6f6e7239ff4fb64585e
Aladser <Aladser@users.noreply.github.com>
2024-02-28 08:26:55 +0900

    обновлено подключение css и js

--- public/template_view.php
+++ public/template_view.php
@@ -10,18 +10,18 @@
     ?>
     <title><?php echo $page_name; ?></title>
     <link href="<?php echo $boostrap_url; ?>" rel="stylesheet" integrity="<?php echo $boostrap_integrity; ?>" crossorigin="anonymous">
-    <link rel="icon" href="http://<?php echo $site_address; ?>/public/images/favicon.ico">
-    <link rel="stylesheet" href="http://<?php echo $site_address; ?>/public/css/template.css">
+    <link rel="icon" href="/public/images/favicon.ico">
     <!-- css -->
+    <link rel="stylesheet" href="/public/css/template.css">
     <?php if (!empty($content_css)) { ?>
             <?php foreach ($content_css as $css) { ?>
-                <link rel="stylesheet" href="<?php echo '/public/css/'.$css; ?>">
+                <link rel="stylesheet" href='<?php echo "/public/css/$css"; ?>'>
             <?php }?>
     <?php } ?>
     <!-- js -->
     <?php if (!empty($content_js)) { ?>
         <?php foreach ($content_js as $script) {?>
-            <script type='text/javascript' src="http://<?php echo $site_address; ?>/public/js/<?php echo $script; ?>" defer></script>
+            <script type='text/javascript' src='<?php echo "/public/js/$script"; ?>' defer></script>
         <?php }?>
     <?php } ?>
 </head>
