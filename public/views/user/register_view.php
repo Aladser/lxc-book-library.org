927389a9eca6900120b9b6ed3bfeb2fd7f7f5159
Aladser <Aladser@users.noreply.github.com>
2024-02-28 11:47:01 +0900

    обновлен sass

--- public/views/user/register_view.php
+++ public/views/user/register_view.php
@@ -3,13 +3,13 @@
 
     <form class='mx-auto mb-3' method="POST" action=<?php echo $routes['store']; ?>>
         <input type="email" name='email' value="<?php echo $data['user']; ?>" 
-        class="d-block w-100 mb-2 p-3" placeholder="Почта" title="Почта" required>
+        class="theme-grey-bgcolor border-1 d-block w-100 mb-2 p-3" placeholder="Почта" title="Почта" required>
 
         <input type="password" name='password'
-        class="d-block w-100 mb-2 p-3" placeholder="Пароль (минимум 3 символа)" title="Пароль" required>
+        class="theme-grey-bgcolor border-1 d-block w-100 mb-2 p-3" placeholder="Пароль (минимум 3 символа)" title="Пароль" required>
 
         <input type="password" name='password_confirm'
-        class="d-block w-100 mb-2 p-3" placeholder="Подтвердите пароль" title="Подтвердите пароль" required>
+        class="theme-grey-bgcolor border-1 d-block w-100 mb-2 p-3" placeholder="Подтвердите пароль" title="Подтвердите пароль" required>
 
         <input type="submit" value="Регистрация" class='button-basic d-block w-100 mb-2'>
         <a href=<?php echo $routes['login']; ?> class='button-basic d-block w-100 mb-2'>Есть учетная запись?</a>
